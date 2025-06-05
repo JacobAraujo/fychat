14bf05de61000e35efe20f9b6b21d42c777a2458
Jacob Alexandre <49577844+JacobAraujo@users.noreply.github.com>
2025-06-04 23:21:29 -0300

    refactor: target path

--- fychat-frontend/vite.config.js
+++ fychat-frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/ws': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8082', 
         ws: true, 
         changeOrigin: true,
       },
